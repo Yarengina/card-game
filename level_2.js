b6c2d86b85d5d9301b6654a6fb4143cf13743fb9
Yarengina <yarenginalyubava@gmail.com>
2022-05-02 23:09:46 +0300

    render pages of levels of difficulty

--- level_2.js
+++ level_2.js
@@ -1,5 +1,46 @@
 function renderLevelTwoScreen() {
-    console.log('Средний уровень');
+    document.body.style.cssText = 'flex-direction: column; gap: 35px;'
+
+    const topField = document.createElement('div');
+    topField.classList.add('top-field');
+    topField.style.cssText = 'display: flex; align-items: center; gap: 50px;'
+    
+    const timer = document.createElement('div');
+    timer.classList.add('timer');
+    const timerMin = document.createElement('span');
+    timerMin.textContent = 'min';
+    timerMin.classList.add('timer__unit');
+    const timerSek = document.createElement('span');
+    timerSek.textContent = 'sek';
+    timerSek.classList.add('timer__unit');
+    const time = document.createElement('p');
+    time.textContent = '00.00';
+    time.classList.add('timer__time');
+    timer.append(timerMin);
+    timer.append(timerSek);
+    timer.append(time);
+
+    const button = document.createElement('button');
+    button.textContent = 'Начать заново';
+    button.classList.add('box__button');
+
+    topField.append(timer);
+    topField.append(button);
+
+    const cardField = document.createElement('div');
+    cardField.classList.add('card-field');
+    cardField.style.cssText = 'width: 440px;'
+    const numberOfCards = 12;
+    for (let i = 0; i < numberOfCards; i++) {
+        const img = document.createElement('img');
+        img.src = "img/card.png";
+        img.classList.add('img');
+        img.style.cssText = 'padding-right: 12px; padding-bottom: 6px;'
+        cardField.append(img);
+    };
+
+    document.body.append(topField);
+    document.body.append(cardField);
 }
 
 window.application.screens['level-2'] = renderLevelTwoScreen;
