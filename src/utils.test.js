9a418983a7e4547e502630e9089e7d4e1f513832
Yarengina <yarenginalyubava@gmail.com>
2022-06-21 12:12:07 +0300

    modified utils.test.js

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,6 +1,5 @@
-import { jest } from '@jest/globals';
-const { describe, it, expect } = require('@jest/globals');
-const { shuffle } = require('./utils');
+import { jest, describe, it, expect } from '@jest/globals';
+import { shuffle } from './utils';
 
 Math.random = jest.fn();
 
@@ -26,33 +25,5 @@ describe('shuffle()', () => {
             { imgSrc: 'Three.png', name: 'Three' },
         ];
         expect(expectedArray).toEqual(array);
-    });
-
-    it('should not change length of array', () => {
-        Math.random.mockImplementation(() => 0.5);
-        shuffle(array);
-        const expectedArray = [
-            { imgSrc: 'Five.png', name: 'Five' },
-            { imgSrc: 'Three.png', name: 'Three' },
-            { imgSrc: 'One.png', name: 'One' },
-            { imgSrc: 'Six.png', name: 'Six' },
-            { imgSrc: 'Four.png', name: 'Four' },
-            { imgSrc: 'Two.png', name: 'Two' },
-        ];
-        expect(expectedArray).toHaveLength(6);
-    });
-
-    it('should contain elements', () => {
-        Math.random.mockImplementation(() => 0.6);
-        shuffle(array);
-        const expectedArray = [
-            { imgSrc: 'Five.png', name: 'Five' },
-            { imgSrc: 'Four.png', name: 'Four' },
-            { imgSrc: 'Three.png', name: 'Three' },
-            { imgSrc: 'One.png', name: 'One' },
-            { imgSrc: 'Two.png', name: 'Two' },
-            { imgSrc: 'Six.png', name: 'Six' },
-        ];
-        expect(expectedArray).toEqual(expect.arrayContaining(array));
     });
 });
